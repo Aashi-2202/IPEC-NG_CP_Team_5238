ebde181cfcff814bcd9a4ea01d71622e4251f46f
Aashi Agarwal <mail.aashiagr@gmail.com>
2026-02-19 15:28:54 +0530

    Redact sensitive email credentials in settings.py
    Updated email configuration with sensitive information redacted.

--- project/settings.py
+++ project/settings.py
@@ -123,6 +123,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'forlaptopuse31@gmail.com'
-EMAIL_HOST_PASSWORD = 'drsmckvzlsvtwjmq'
+EMAIL_HOST_USER = ###@gmail.com
+EMAIL_HOST_PASSWORD = #####
+
 
